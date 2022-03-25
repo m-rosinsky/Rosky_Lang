508fb40659a58e07522130a68d48db034884dc97
Mike Rosinsky <56515138+m-rosinsky@users.noreply.github.com>
2022-03-25 15:18:45 -0400

    Add files via upload

--- src/lexer.cpp
+++ src/lexer.cpp
@@ -40,7 +40,7 @@
 void tokenize_src(const std::unique_ptr<Src_T>& __src) {
 
     // Create a deque to hold the token table.
-    std::deque<std::unique_ptr<Token_T>> tokens;
+    std::deque<std::shared_ptr<Token_T>> tokens;
 
     // Temporary string to hold each token.
     std::string token;
@@ -53,8 +53,31 @@ void tokenize_src(const std::unique_ptr<Src_T>& __src) {
     size_t colnum = 1;
     size_t linenum = 1;
 
+    // Flag to indicate if we are in a comment block.
+    bool in_comment = false;
+
     // Iterate through input src.
     while (idx < __src->_data.size()) {
+
+        // If in comment, ignore until new line.
+        if (in_comment) {
+            colnum++;
+            if (__src->_data[idx] == NEWLINE) {
+                linenum++;
+                colnum = 1;
+                in_comment = false;
+            }
+            idx++;
+            continue;
+        }
+
+        // Comment character '#'
+        if (__src->_data[idx] == COMMENT) {
+            in_comment = true;
+            colnum++;
+            idx++;
+            continue;
+        }
 
         // Ignore whitespace.
         if (is_whitespace(__src->_data[idx])) {
@@ -72,7 +95,7 @@ void tokenize_src(const std::unique_ptr<Src_T>& __src) {
             token += __src->_data[idx];
 
             // Add the delimiter to the token table.
-            tokens.push_back(std::make_unique<Token_T>
+            tokens.push_back(std::make_shared<Token_T>
                 (token, TOKEN_DELIM, colnum, linenum));
 
             // Reset the token and continue.
@@ -89,8 +112,8 @@ void tokenize_src(const std::unique_ptr<Src_T>& __src) {
             token += __src->_data[idx];
 
             // Push the token into the table.
-            tokens.push_back(std::make_unique<Token_T>
-                (token, TOKEN_OP, colnum, linenum));
+            tokens.push_back(std::make_shared<Token_T>
+                (token, TOKEN_OP_DLR, colnum, linenum));
 
             // Reset the token and continue.
             token = "";
@@ -103,18 +126,67 @@ void tokenize_src(const std::unique_ptr<Src_T>& __src) {
         // Number
         if (is_num(__src->_data[idx])) {
 
-            // Bookmark the start column number of the number.
+            // Bookmark the start column number of the token.
             size_t start_col = colnum;
 
-            // Collect all numbers in sequence.
-            while (is_num(__src->_data[idx])) {
+            // This flag holds whether the number is an integer
+            // or float
+            bool is_int = true;
+
+            // Collect all numbers in sequence (including '.')
+            while (is_num(__src->_data[idx]) ||
+                   (__src->_data[idx] == '.')) {
+
+                // If we collect a '.', set the flag.
+                // If the flag is already set, throw an error.
+                if (__src->_data[idx] == '.') {
+                    
+                    if (is_int == false) {
+                        token += '.';
+                        throw_error(ERR_UNEXP_TOKEN, token, colnum, linenum);
+                    }
+                    is_int = false;
+
+                }
+
+                token += __src->_data[idx++];
+                colnum++;
+            }
+
+            // If the last character in the token is a decimal,
+            // throw an error.
+            if (token[token.size()-1] == '.') {
+                throw_error(ERR_UNEXP_TOKEN, token, colnum, linenum);
+            }
+
+            // Determine the token type based on the flag.
+            TOKEN_TYPE t = is_int ? TOKEN_LIT_INT : TOKEN_LIT_FLOAT;
+
+            // Push the token into the table.
+            tokens.push_back(std::make_shared<Token_T>
+                             (token, t, start_col, linenum));
+
+            // Reset the token and continue.
+            token = "";
+            continue;
+
+        }
+
+        // Alphanumeric
+        if (is_alpha(__src->_data[idx])) {
+
+            // Bookmark the start column number of the token.
+            size_t start_col = colnum;
+
+            // Collect all the alphanumeric characters in sequence.
+            while (is_alphanum(__src->_data[idx])) {
                 token += __src->_data[idx++];
                 colnum++;
             }
 
             // Push the token into the table.
-            tokens.push_back(std::make_unique<Token_T>
-                (token, TOKEN_LITERAL, start_col, linenum));
+            tokens.push_back(std::make_shared<Token_T>
+                             (token, TOKEN_SYMBOL, start_col, linenum));
 
             // Reset the token and continue.
             token = "";
@@ -130,10 +202,13 @@ void tokenize_src(const std::unique_ptr<Src_T>& __src) {
 
     }
 
+    // ***DEBUG***
+    // for (auto& tok : tokens) {
+    //     std::cout << tok->_token << "\t| " << TOKEN_STRINGS[tok->_type] << std::endl;
+    // }
+
     // Send the token table to the parser.
-    for (auto& tok : tokens) {
-        std::cout << tok->_token << "\t| " << TOKEN_STRINGS[tok->_type] << "\t| " << tok->_linenum << " " << tok->_colnum << std::endl;
-    }
+    parse(tokens);
     return;
 
 }
