2d75351509d6e5c57913ed3b0c23e9a4b50eafbc
Mike Rosinsky <56515138+m-rosinsky@users.noreply.github.com>
2022-03-25 15:19:16 -0400

    Add files via upload

---
+++ src/objects/rosky_int.cpp
@@ -0,0 +1,73 @@
+
+/******************************************************************************/
+//
+//  Source Name:                rosky_int.hpp
+//
+//  Description:                This file contains the class definition for
+//                              the built in integer type.
+// 
+//                              The underlying data type is a long, rather
+//                              than an int.
+//
+//  Dependencies:               RoskyInterface
+//
+//  Classes:                    RoskyInt
+//
+//  Inherited Subprograms:      get_type_id
+//                              get_type_string
+//                              to_int
+//                              to_string
+//                              add_op
+//
+//  Exported Subprograms:       None
+//                              
+/******************************************************************************/
+
+#include "../../includes/objects/rosky_int.hpp"
+
+/******************************************************************************/
+
+// Type information.
+OBJ_TYPES RoskyInt::get_type_id() const noexcept {
+    return OBJ_INT;
+}
+
+std::string RoskyInt::get_type_string() const noexcept {
+    return "int";
+}
+
+/******************************************************************************/
+
+// Casting.
+long RoskyInt::to_int() const noexcept {
+    return _data;
+}
+
+std::string RoskyInt::to_string() const noexcept {
+    return std::to_string(_data);
+}
+
+/******************************************************************************/
+
+// Arithmetic operators.
+std::shared_ptr<RoskyInterface> RoskyInt::add_op(const std::shared_ptr<RoskyInterface>& __r) const noexcept {
+
+    if (__r->get_type_id() == OBJ_INT) {
+        return std::make_shared<RoskyInt>(_data + __r->to_int());
+    }
+
+    return nullptr;
+
+}
+
+std::shared_ptr<RoskyInterface> RoskyInt::mul_op(const std::shared_ptr<RoskyInterface>& __r) const noexcept {
+
+    if (__r->get_type_id() == OBJ_INT) {
+        return std::make_shared<RoskyInt>(_data * __r->to_int());
+    }
+
+    return nullptr;
+
+}
+
+/******************************************************************************/
